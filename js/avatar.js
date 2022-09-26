003ccd8204e81b36a638d0755c351e9618235500
michel4alves <michelsantosa@gmail.com>
2022-09-25 21:15:17 -0300

    first part of about

--- js/avatar.js
+++ js/avatar.js
@@ -9,3 +9,18 @@ document.querySelector('body').addEventListener('mousemove', eyeball);
                 eye.style.transform = "rotate("+ rot +"deg)";
             })
         }
+
+function menu() {
+    let menu = document.querySelector('.menu')
+    menu.classList.toggle('active')
+}
+
+function exitMenu() {
+    let menu = document.querySelector('.menu')
+    menu.classList.remove('active')
+}
+
+function link() {
+    let menu = document.querySelector('.menu')
+    menu.classList.remove('active')
+}
---
+++ js/button_jq.js
@@ -0,0 +1,13 @@
+$(document).ready(function(e) {
+    $('.cvbtn').on('mouseenter', function(e) {
+        x = e.pageX - $(this).offset().left
+        y = e.pageY - $(this).offset().top
+        $(this).find('span').css({top:y, left:x})
+    })
+    $('.cvbtn').on('mouseout', function(e) {
+        x = e.pageX - $(this).offset().left
+        y = e.pageY - $(this).offset().top
+        $(this).find('span').css({top:y, left:x})
+    })
+})
+
