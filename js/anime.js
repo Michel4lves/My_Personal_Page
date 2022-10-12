59e2d8c4cb91ddbb0fa8cec83d46a2c6d4098b49
michel4alves <michelsantosa@gmail.com>
2022-10-12 01:40:02 -0300

    add animation on scroll

---
+++ js/anime.js
@@ -0,0 +1,45 @@
+// Lib Lodash
+const debounce = function(func, wait, immediate) {
+    let timeout;
+    return function(...args) {
+    const context = this;
+    const later = function () {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+    };
+    const callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+    };
+};
+
+
+
+var content = document.querySelector('.page-container')
+
+let target = document.querySelectorAll('[data-anime]')
+var telaAlto = ((window.innerHeight * 2) / 3)
+var telaBaixo = ((window.innerHeight * 3) / 5)
+
+function animeScroll() {
+    let windowTop = window.pageYOffset - telaAlto
+    let windowBottom = window.pageYOffset + telaBaixo
+
+    target.forEach(function(element) {
+        if ((windowBottom > element.offsetTop) && (windowTop < element.offsetTop)) {
+            element.classList.add('animate')
+        }else{
+            element.classList.remove('animate')
+        }
+    })
+}
+
+if (target.length) {
+    window.addEventListener('scroll',debounce(function() {
+        animeScroll()
+        let card1 = document.querySelector('.card1')
+        card1.classList.remove('card1')
+    }, 100))
+}
+
---
+++ js/reloadCss.js
@@ -0,0 +1,14 @@
+refreshCSS = () => {
+    let links = document.getElementsByTagName('link');
+    for (let i = 0; i < links.length; i++) {
+        if (links[i].getAttribute('rel') == 'stylesheet') {
+            let href = links[i].getAttribute('href')
+                                    .split('?')[0];
+            
+            let newHref = href + '?version=' 
+                        + new Date().getMilliseconds();
+            
+            links[i].setAttribute('href', newHref);
+        }
+    }
+}
