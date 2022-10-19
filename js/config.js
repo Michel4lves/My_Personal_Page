5555cc24523c41e8dd67b9c458d72870f805fd4b
michel4alves <michelsantosa@gmail.com>
2022-10-19 01:36:48 -0300

    add color blue

--- js/config.js
+++ js/config.js
@@ -4,6 +4,58 @@ configBtn.addEventListener('click', function() {
     menuConfig.classList.toggle('active')
 })
 
+// BLUE
+document.querySelector('.blue').addEventListener('click', function() {
+
+    document.querySelectorAll('.color-set').forEach(element => {
+        element.style.color = '#00a2ff'
+    });
+
+    document.styleSheets[0].cssRules[6].style.background = '#00a2ff' // index
+    document.querySelector('.logo').src = 'imgs/logos/logo-az.png'
+    document.styleSheets[0].cssRules[11].style.background = '#00a2ff'
+    document.styleSheets[0].cssRules[12].style.background = '#00a2ff'
+
+    document.querySelector('.logo-menu').src = 'imgs/logos/logo-az.png' // menu
+    document.styleSheets[1].cssRules[6].style.background = '#00a2ff'
+    document.styleSheets[1].cssRules[7].style.background = '#00a2ff'
+    document.styleSheets[1].cssRules[11].style.color = '#00a2ff'
+    document.styleSheets[1].cssRules[13].style.color = '#00a2ff'
+    document.querySelectorAll('.menu-list').forEach(option => {
+        option.addEventListener('mouseenter', function() {
+            option.style.color = '#fff'
+        })
+        option.addEventListener('mouseout', function() {
+            option.style.color = '#00a2ff'
+        })
+    })
+
+    document.querySelector('.me').src = 'imgs/avatar/eu-az.png' // intro
+    // document.styleSheets[2].cssRules[1].style.background = 'linear-gradient(80deg, #72C042 30%, #171717 45%)'
+    document.styleSheets[2].cssRules[3].style.border = '3px solid #00a2ff'
+
+    document.styleSheets[3].cssRules[9].style.border = '1px solid #00a2ff' // about
+    document.styleSheets[3].cssRules[11].style.background = '#00a2ff'
+    document.styleSheets[3].cssRules[28].style.stroke = '#00a2ff'
+    document.styleSheets[3].cssRules[32].style.background = '#00a2ff'
+    document.styleSheets[3].cssRules[32].style.boxShadow = '0 0 10px #00a2ff, 0 0 30px #00a2ff'
+
+    document.styleSheets[4].cssRules[7].style.border = '1px solid #00a2ff'  // portfolio
+    document.styleSheets[4].cssRules[9].style.color = '#00a2ff'
+    document.styleSheets[4].cssRules[23].style.background = '#00a2ff'
+    document.styleSheets[4].cssRules[29].style.border = '1px solid #00a2ff'
+    
+    document.styleSheets[5].cssRules[4].style.background = '#00a2ff' // contact
+    document.styleSheets[5].cssRules[14].style.border = '2px solid #00a2ff'
+    document.styleSheets[5].cssRules[23].style.color = '#00a2ff'
+    document.styleSheets[5].cssRules[24].style.background = '#00a2ff'
+    document.styleSheets[5].cssRules[24].style.border = '1px solid #00a2ff'
+    
+    document.styleSheets[6].cssRules[0].style.background = '#00a2ff' // transition
+
+})
+
+
 // GREEN
 document.querySelector('.green').addEventListener('click', function() {
 
@@ -31,7 +83,7 @@ document.querySelector('.green').addEventListener('click', function() {
     })
 
     document.querySelector('.me').src = 'imgs/avatar/eu-vd.png' // intro
-    document.styleSheets[2].cssRules[1].style.background = 'linear-gradient(80deg, #72C042 30%, #171717 45%)'
+    // document.styleSheets[2].cssRules[1].style.background = 'linear-gradient(80deg, #72C042 30%, #171717 45%)'
     document.styleSheets[2].cssRules[3].style.border = '3px solid #72C042'
 
     document.styleSheets[3].cssRules[9].style.border = '1px solid #72C042' // about
