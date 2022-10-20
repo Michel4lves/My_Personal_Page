8ac6b0cd545243f78d7036d237d06bf9094eec35
michel4alves <michelsantosa@gmail.com>
2022-10-20 20:50:47 -0300

    contacts correct form

--- js/message.js
+++ js/message.js
@@ -3,10 +3,16 @@ function Enviar() {
     let last = document.getElementById("last-name")
     let email = document.getElementById("email")
     let mobile = document.getElementById("mobile-number")
+    let message = document.getElementById("message")
     if (nome.value != "" && last.value != "" && email.value != "" && mobile.value != "") {
         let sendBtn = document.querySelector('.send')
         sendBtn.value = 'Obrigado!!'
         sendBtn.style.background = '#121212'
+        nome.value = ''
+        last.value = ''
+        email.value = ''
+        mobile.value = ''
+        message.value = ''
     }else{
         alert('Os campos: Nome, Sobrenome, Email e Telefone são obrigatórios')
     }
