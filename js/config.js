10e20f8f5f6bad794f586dadebb84dc79aeb60db
michel4alves <michelsantosa@gmail.com>
2022-10-20 00:37:45 -0300

    Added Colors config

--- js/config.js
+++ js/config.js
@@ -4,106 +4,101 @@ configBtn.addEventListener('click', function() {
     menuConfig.classList.toggle('active')
 })
 
-// BLUE
-document.querySelector('.blue').addEventListener('click', function() {
+let colors = document.querySelectorAll('.color-page')
+colors.forEach(element => {
+    element.addEventListener('click', function(event) {
+        let id = event.target.id
+        
+        switch(id) {
+            case 'blue':
+                var color = '#00a2ff'
+                document.querySelector('.logo').src = 'imgs/logos/logo-az.png' // index
+                document.querySelector('.me').src = 'imgs/avatar/eu-az.png' // intro
+                document.querySelector('.logo-menu').src = 'imgs/logos/logo-az.png' // menu
+                break
+            case 'green':
+                var color = '#72C042'
+                document.querySelector('.logo').src = 'imgs/logos/logo-vd.png' // index
+                document.querySelector('.me').src = 'imgs/avatar/eu-vd.png' // intro
+                document.querySelector('.logo-menu').src = 'imgs/logos/logo-vd.png' // menu
+                break
+            case 'orange':
+                var color = '#ab741f'
+                document.querySelector('.logo').src = 'imgs/logos/logo-la.png' // index
+                document.querySelector('.me').src = 'imgs/avatar/eu-la.png' // intro
+                document.querySelector('.logo-menu').src = 'imgs/logos/logo-la.png' // menu
+                break
+            case 'purple':
+                var color = '#8d4194'
+                document.querySelector('.logo').src = 'imgs/logos/logo-rx.png' // index
+                document.querySelector('.me').src = 'imgs/avatar/eu-rx.png' // intro
+                document.querySelector('.logo-menu').src = 'imgs/logos/logo-rx.png' // menu
+                break
+            case 'red':
+                var color = '#ff4138'
+                document.querySelector('.logo').src = 'imgs/logos/logo-vm.png' // index
+                document.querySelector('.me').src = 'imgs/avatar/eu-vm.png' // intro
+                document.querySelector('.logo-menu').src = 'imgs/logos/logo-vm.png' // menu
+                break
+            case 'yellow':
+                var color = '#fff200'
+                document.querySelector('.logo').src = 'imgs/logos/logo-am.png' // index
+                document.querySelector('.me').src = 'imgs/avatar/eu-am.png' // intro
+                document.querySelector('.logo-menu').src = 'imgs/logos/logo-am.png' // menu
+                break
+        }
 
-    document.querySelectorAll('.color-set').forEach(element => {
-        element.style.color = '#00a2ff'
-    });
+        document.querySelectorAll('.color-set').forEach(element => {
+            element.style.color = color
+        });
 
-    document.styleSheets[0].cssRules[6].style.background = '#00a2ff' // index
-    document.querySelector('.logo').src = 'imgs/logos/logo-az.png'
-    document.styleSheets[0].cssRules[11].style.background = '#00a2ff'
-    document.styleSheets[0].cssRules[12].style.background = '#00a2ff'
+// index
+        document.styleSheets[0].cssRules[6].style.background = color
+        document.styleSheets[0].cssRules[11].style.background = color
+        document.styleSheets[0].cssRules[12].style.background = color
 
-    document.querySelector('.logo-menu').src = 'imgs/logos/logo-az.png' // menu
-    document.styleSheets[1].cssRules[6].style.background = '#00a2ff'
-    document.styleSheets[1].cssRules[7].style.background = '#00a2ff'
-    document.styleSheets[1].cssRules[11].style.color = '#00a2ff'
-    document.styleSheets[1].cssRules[13].style.color = '#00a2ff'
-    document.querySelectorAll('.menu-list').forEach(option => {
-        option.addEventListener('mouseenter', function() {
-            option.style.color = '#fff'
+// menu
+        document.styleSheets[1].cssRules[6].style.background = color
+        document.styleSheets[1].cssRules[7].style.background = color
+        document.styleSheets[1].cssRules[11].style.color = color
+        document.styleSheets[1].cssRules[13].style.color = color
+
+// intro
+        document.styleSheets[2].cssRules[3].style.border = `3px solid ${color}`
+
+// about
+        document.styleSheets[3].cssRules[9].style.border = `1px solid ${color}`
+        document.styleSheets[3].cssRules[11].style.background = color
+        document.styleSheets[3].cssRules[28].style.stroke = color
+        document.styleSheets[3].cssRules[32].style.background = color
+        document.styleSheets[3].cssRules[32].style.boxShadow = `0 0 10px #72C042, 0 0 30px ${color}`
+
+// portfolio
+        document.styleSheets[4].cssRules[7].style.border = `1px solid ${color}`
+        document.styleSheets[4].cssRules[9].style.color = color
+        document.styleSheets[4].cssRules[23].style.background = color
+        document.styleSheets[4].cssRules[29].style.border = `1px solid ${color}`
+
+// contact
+        document.styleSheets[5].cssRules[4].style.background = color
+        document.styleSheets[5].cssRules[14].style.border = `2px solid ${color}`
+        document.styleSheets[5].cssRules[23].style.color = color
+        document.styleSheets[5].cssRules[24].style.background = color
+        document.styleSheets[5].cssRules[24].style.border = `1px solid ${color}`
+
+// transition
+        document.styleSheets[6].cssRules[0].style.background = color
+
+
+        document.querySelectorAll('.menu-list').forEach(option => {
+            option.addEventListener('mouseenter', function() {
+                option.style.color = '#fff'
+            })
+            option.addEventListener('mouseout', function() {
+                option.style.color = color
+            })
         })
-        option.addEventListener('mouseout', function() {
-            option.style.color = '#00a2ff'
-        })
+
     })
-
-    document.querySelector('.me').src = 'imgs/avatar/eu-az.png' // intro
-    // document.styleSheets[2].cssRules[1].style.background = 'linear-gradient(80deg, #72C042 30%, #171717 45%)'
-    document.styleSheets[2].cssRules[3].style.border = '3px solid #00a2ff'
-
-    document.styleSheets[3].cssRules[9].style.border = '1px solid #00a2ff' // about
-    document.styleSheets[3].cssRules[11].style.background = '#00a2ff'
-    document.styleSheets[3].cssRules[28].style.stroke = '#00a2ff'
-    document.styleSheets[3].cssRules[32].style.background = '#00a2ff'
-    document.styleSheets[3].cssRules[32].style.boxShadow = '0 0 10px #00a2ff, 0 0 30px #00a2ff'
-
-    document.styleSheets[4].cssRules[7].style.border = '1px solid #00a2ff'  // portfolio
-    document.styleSheets[4].cssRules[9].style.color = '#00a2ff'
-    document.styleSheets[4].cssRules[23].style.background = '#00a2ff'
-    document.styleSheets[4].cssRules[29].style.border = '1px solid #00a2ff'
-    
-    document.styleSheets[5].cssRules[4].style.background = '#00a2ff' // contact
-    document.styleSheets[5].cssRules[14].style.border = '2px solid #00a2ff'
-    document.styleSheets[5].cssRules[23].style.color = '#00a2ff'
-    document.styleSheets[5].cssRules[24].style.background = '#00a2ff'
-    document.styleSheets[5].cssRules[24].style.border = '1px solid #00a2ff'
-    
-    document.styleSheets[6].cssRules[0].style.background = '#00a2ff' // transition
-
-})
-
-
-// GREEN
-document.querySelector('.green').addEventListener('click', function() {
-
-    document.querySelectorAll('.color-set').forEach(element => {
-        element.style.color = '#72C042'
-    });
-
-    document.styleSheets[0].cssRules[6].style.background = '#72C042' // index
-    document.querySelector('.logo').src = 'imgs/logos/logo-vd.png'
-    document.styleSheets[0].cssRules[11].style.background = '#72C042'
-    document.styleSheets[0].cssRules[12].style.background = '#72C042'
-
-    document.querySelector('.logo-menu').src = 'imgs/logos/logo-vd.png' // menu
-    document.styleSheets[1].cssRules[6].style.background = '#72C042'
-    document.styleSheets[1].cssRules[7].style.background = '#72C042'
-    document.styleSheets[1].cssRules[11].style.color = '#72C042'
-    document.styleSheets[1].cssRules[13].style.color = '#72C042'
-    document.querySelectorAll('.menu-list').forEach(option => {
-        option.addEventListener('mouseenter', function() {
-            option.style.color = '#fff'
-        })
-        option.addEventListener('mouseout', function() {
-            option.style.color = '#72C042'
-        })
-    })
-
-    document.querySelector('.me').src = 'imgs/avatar/eu-vd.png' // intro
-    // document.styleSheets[2].cssRules[1].style.background = 'linear-gradient(80deg, #72C042 30%, #171717 45%)'
-    document.styleSheets[2].cssRules[3].style.border = '3px solid #72C042'
-
-    document.styleSheets[3].cssRules[9].style.border = '1px solid #72C042' // about
-    document.styleSheets[3].cssRules[11].style.background = '#72C042'
-    document.styleSheets[3].cssRules[28].style.stroke = '#72C042'
-    document.styleSheets[3].cssRules[32].style.background = '#72C042'
-    document.styleSheets[3].cssRules[32].style.boxShadow = '0 0 10px #72C042, 0 0 30px #72C042'
-
-    document.styleSheets[4].cssRules[7].style.border = '1px solid #72C042'  // portfolio
-    document.styleSheets[4].cssRules[9].style.color = '#72C042'
-    document.styleSheets[4].cssRules[23].style.background = '#72C042'
-    document.styleSheets[4].cssRules[29].style.border = '1px solid #72C042'
-    
-    document.styleSheets[5].cssRules[4].style.background = '#72C042' // contact
-    document.styleSheets[5].cssRules[14].style.border = '2px solid #72C042'
-    document.styleSheets[5].cssRules[23].style.color = '#72C042'
-    document.styleSheets[5].cssRules[24].style.background = '#72C042'
-    document.styleSheets[5].cssRules[24].style.border = '1px solid #72C042'
-    
-    document.styleSheets[6].cssRules[0].style.background = '#72C042' // transition
-
 })
 
